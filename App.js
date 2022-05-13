40dc22276ba6579e344fe321e53ffce50c018851
miryo38 <miryo38@naver.com>
2022-05-13 15:43:33 +0900

    update

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import { StatusBar,SafeAreaView,StyleSheet } from 'react-native';
 import React from 'react';
 import Providers from './src/utils';
-
+import { LogBox } from "react-native";
 import store from './store';
 import {Provider} from "react-redux";
 import { theme } from './src/Chat/ChatTheme';
 const App = () => {
 
+  LogBox.ignoreLogs([
+    'Animated: `useNativeDriver` was not specified.',
+  ]);
 
-
+  LogBox.ignoreLogs(["EventEmitter.removeListener"]);
   return (
     <SafeAreaView style={styles.container}>
   <StatusBar style="light" backgroundColor='orange' />
